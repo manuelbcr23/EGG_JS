55e929bd746f519fd59bb3a8d03a68485d9c6f86
manuelbcr23 <manuelbelenocastellar@hotmail.com>
2024-04-16 20:46:36 -0500

    Actualización calculator

--- calculator/index.js
+++ calculator/index.js
@@ -20,5 +20,14 @@ const mensajeFinal = "El resultado de las operaciones es: "+resultadoMenosDiez;
 console.log(mensajeFinal);
 alert(mensajeFinal);
 
+/* const cantidad = Number(prompt("¿Cuántos números desea sumar?"));
+let sumatoria = 0;
+for (let index = 1; index <= cantidad; index++) {
+    const numeroAsumar = Number(prompt("Ingrese un número a sumar"));
+    sumatoria = sumatoria + numeroAsumar;
+
+    alert (sumatoria)
+  } */
+
 
 
